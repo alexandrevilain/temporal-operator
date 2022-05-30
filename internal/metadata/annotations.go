654ed8b929829f85826b478ac10d2accd72120ca
Alexandre Vilain <alexandre.vilain@me.com>
2022-05-30 19:17:46 +0200

    Add support for webui

--- internal/metadata/annotations.go
+++ internal/metadata/annotations.go
@@ -18,6 +18,6 @@
 package metadata
 
 // GetAnnotations returns service annotations.
-func GetAnnotations(_ string, annotations map[string]string) map[string]string {
-	return annotations
+func GetAnnotations(_ string, annotations ...map[string]string) map[string]string {
+	return Merge(annotations...)
 }
