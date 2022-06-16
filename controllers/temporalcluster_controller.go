9b211541ae8e4575eecdbe3fe93f364cba66c4f7
Alexandre Vilain <alexandre.vilain@me.com>
2022-06-16 09:31:14 +0200

    fix: rbac for ingresses and services

--- controllers/temporalcluster_controller.go
+++ controllers/temporalcluster_controller.go
@@ -55,11 +55,11 @@ type TemporalClusterReconciler struct {
 	PersistenceManager *persistence.Manager
 }
 
-//+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update
+//+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;delete
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;delete
 //+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;create;update
 //+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch
-//+kubebuilder:rbac:groups="networking",resources=ingress,verbs=get;list;watch;create;update
+//+kubebuilder:rbac:groups="networking.k8s.io",resources=ingresses,verbs=get;list;watch;create;update;delete
 //+kubebuilder:rbac:groups=apps.alexandrevilain.dev,resources=temporalclusters,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=apps.alexandrevilain.dev,resources=temporalclusters/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=apps.alexandrevilain.dev,resources=temporalclusters/finalizers,verbs=update
