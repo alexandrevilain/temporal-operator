9bf6c3742b62d6a8eb9aab1486b483c2a0a8cfe9
Alexandre Vilain <alexandre.vilain@me.com>
2022-07-04 16:43:09 +0200

    Add cert-manager install in e2e test bootstrap + fix rbac

--- controllers/temporalcluster_controller.go
+++ controllers/temporalcluster_controller.go
@@ -71,6 +71,7 @@ type TemporalClusterReconciler struct {
 //+kubebuilder:rbac:groups=apps.alexandrevilain.dev,resources=temporalclusters,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=apps.alexandrevilain.dev,resources=temporalclusters/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=apps.alexandrevilain.dev,resources=temporalclusters/finalizers,verbs=update
+//+kubebuilder:rbac:groups="cert-manager.io",resources=certificates;issuers,verbs=get;list;watch;create;update;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
